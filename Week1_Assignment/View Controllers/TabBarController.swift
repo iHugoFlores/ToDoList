a67d39a053597d6300266791a5361f3964a68371
Hugo Flores <hfp.ing2@gmail.com>
2020-03-22 14:52:38 -0400

    Save data on background

--- Week1_Assignment/View Controllers/TabBarController.swift
+++ Week1_Assignment/View Controllers/TabBarController.swift
@@ -10,12 +10,45 @@ import UIKit
 
 class TabBarController: UITabBarController {
     
-    var taskItems = TaskItem.getMockData()
+    var taskItems = [TaskItem]()// TaskItem.getMockData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        taskItems[0].setDone()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(UIApplicationDelegate.applicationDidEnterBackground(_:)),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil)
+        
+        do {
+            // Try to load from persistence
+            self.taskItems = try [TaskItem].readFromPersistence()
+            print("Items read from storage: \(taskItems.count)")
+        } catch let error as NSError {
+            if error.domain == NSCocoaErrorDomain && error.code == NSFileReadNoSuchFileError {
+                NSLog("No persistence file found, not necesserially an error...")
+            } else {
+                let alert = UIAlertController(
+                    title: "Error",
+                    message: "Could not load the to-do items!",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                NSLog("Error loading from persistence: \(error)")
+            }
+        }
     }
+    
+    @objc public func applicationDidEnterBackground(_ notification: NSNotification) {
+        do {
+            print("Wiriting items to storage")
+            try taskItems.writeToPersistence()
+        } catch let error {
+            NSLog("Error writing to persistence: \(error)")
+        }
+    }
+
     
     func setTaskToDone(id row: IndexPath) {
         let item = taskItems[row.row]
